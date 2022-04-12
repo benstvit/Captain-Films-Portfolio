2bef858e37bf7d3959bb8f09ad3090549a245c82
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-04-12 18:08:57 +0200

    work in progress with track upload

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,9 +1,21 @@
 class TracksController < ApplicationController
-
   def new
+    @track = Track.new
+    @albums = Track.all
+    authorize @albums
   end
 
   def create
+    @track = Track.new(tracks_params)
+    @album = Album.where(id: params[:album_id])
+    @track.album = @album
+    authorize @track
+    authorize @album
+    if @track.save!
+      redirect_to album_path(@album)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -12,6 +24,6 @@ class TracksController < ApplicationController
   private
 
   def tracks_params
-    params.require(:track).permit(:title, :audio)
+    params.require(:track).permit(:title, :audio, :album_id)
   end
 end
