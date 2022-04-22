159a4d1743c09530d383c88edd3539d6eb6ff4ab
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-04-22 10:30:01 +0100

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_125500) do
+ActiveRecord::Schema.define(version: 2022_04_22_085242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,11 @@ ActiveRecord::Schema.define(version: 2022_04_11_125500) do
     t.string "facebook_url"
     t.string "youtube_url"
     t.index ["user_id"], name: "index_bands_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "photos", force: :cascade do |t|
