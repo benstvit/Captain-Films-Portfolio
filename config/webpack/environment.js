e50a9ea26edb47c7980e56f2aead22ab6057f8cb
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-04-28 12:25:35 +0200

    vue installed not working yet

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
